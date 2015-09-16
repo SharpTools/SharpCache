0363855b6e9fa58c3a735a70b9e74af048d6b014
Andre Carlucci <andrecarlucci@gmail.com>
2015-09-15 22:26:58 -0300

    Version 1.0

---
+++ SharpCache.Tests/MemCacheTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SharpCache.Tests {
+    public class MemCacheTests {
+
+        private MemCache<int, string> _cache;
+        
+        [SetUp]
+        public void SetUp() {
+            _cache = new MemCache<int, string>(3, TimeSpan.FromMilliseconds(1000));
+        }
+
+        [Test]
+        public void Should_add_simple_item() {
+            _cache.Add(1, "1");
+            Assert.AreEqual("1", _cache.Get(1));
+        }
+
+        [Test]
+        public void Should_remove_oldest_after_limit() {
+            _cache.Add(1, "a");
+            _cache.Add(2, "b");
+            _cache.Add(3, "c");
+            _cache.Add(4, "d");
+            Assert.AreEqual("b", _cache.Get(2));
+            Assert.AreEqual("c", _cache.Get(3));
+            Assert.AreEqual("d", _cache.Get(4));
+            Assert.AreEqual(null, _cache.Get(1));
+        }
+
+        [Test]
+        public async Task Should_not_return_expired() {
+            _cache = new MemCache<int, string>(3, TimeSpan.FromMilliseconds(100));
+            _cache.Add(1, "a");
+            await Task.Delay(100);
+            Assert.AreEqual(null, _cache.Get(1));
+        }
+    }
+}
---
+++ SharpCache/MemCache.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpCache {
+    public class MemCache<TK, TV> where TV : class {
+        private readonly int _maxItems;
+        private readonly TimeSpan _expirationSpan;
+        private static object _syncRoot = new object();
+
+        private Dictionary<TK, CachedItem> _dic = new Dictionary<TK, CachedItem>();
+
+        public MemCache(int maxItems, TimeSpan expirationSpan) {
+            _maxItems = maxItems;
+            _expirationSpan = expirationSpan;
+        }
+
+        public void Add(TK key, TV value) {
+            lock (_syncRoot) {
+                if (!_dic.ContainsKey(key) && _dic.Count >= _maxItems) {
+                    RemoveOldest();
+                }
+                _dic[key] = new CachedItem(value, _expirationSpan);
+            }
+        }
+
+        public TV Get(TK key) {
+            lock (_syncRoot) {
+                if (!_dic.ContainsKey(key)) {
+                    return null;
+                }
+                var cachedItem = _dic[key];
+                if (cachedItem.IsValid()) {
+                    cachedItem.Touch();
+                    return cachedItem.Item;
+                }
+                _dic.Remove(key);
+                return null;
+            }
+        }
+
+        private void RemoveOldest() {
+            var oldest = DateTime.MaxValue;
+            var key = default(TK);
+            foreach (var keyValue in _dic) {
+                if (keyValue.Value.LastUsed < oldest) {
+                    oldest = keyValue.Value.LastUsed;
+                    key = keyValue.Key;
+                }
+            }
+            if (key != null) {
+                _dic.Remove(key);
+            }
+        }
+
+        private class CachedItem {
+            private readonly TimeSpan _expiration;
+            public DateTime LastUsed { get; private set; }
+            public TV Item { get; }
+
+            public CachedItem(TV item, TimeSpan expiration) {
+                _expiration = expiration;
+                Item = item;
+                Touch();
+            }
+
+            public bool IsValid() {
+                return (DateTime.Now - LastUsed) < _expiration;
+            }
+
+            public void Touch() {
+                LastUsed = DateTime.Now;
+            }
+        }
+    }
+}
