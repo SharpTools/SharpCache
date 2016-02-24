880aa9492e759e52ab75cd549a1d72cd6501e8ff
andrecarlucci <andrecarlucci@gmail.com>
2016-02-24 13:56:05 -0300

    adding tests using vnext

---
+++ Tests/SharpCache.Tests/Program.cs
@@ -0,0 +1,9 @@
+﻿using NUnitLite;
+
+namespace SharpCache.Tests {
+    public class Program {
+        public static int Main(string[] args) {
+            return new AutoRun().Execute(args);
+        }
+    }
+}
